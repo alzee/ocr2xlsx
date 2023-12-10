35a9d32431eb39f4a1f21ef0576cf4ac800a6fd6
Al Zee <z@alz.ee>
2023-12-10 14:53:48 +0800

    nth

--- a.py
+++ a.py
@@ -40,28 +40,36 @@ class Sample:
     def main(
         args: List[str],
     ) -> None:
-        imgs = os.listdir()
-        for i in imgs:
-            kind = filetype.guess(i)
-            # if kind is not None and (kind.extension == 'jpg' or kind.extension == 'png'):
-            if kind is None:
-                continue
+        # files = os.listdir()
+        files = os.scandir()
+        for i in files:
+            if i.is_file():
+                kind = filetype.guess(i.name)
+                # if kind is not None and (kind.extension == 'jpg' or kind.extension == 'png'):
+                if kind is None:
+                    continue
 
-            print(i)
-            client = Sample.create_client('LTAI5tBmKAjcNE7mpFiSW7n4', 'qOp2XtV5PQZjPh5O28YctTRb0x8B0n')
-            body_stream = StreamClient.read_from_file_path(i)
-            recognize_general_request = ocr_api_20210707_models.RecognizeGeneralRequest(
-                body=body_stream
-            )
-            runtime = util_models.RuntimeOptions()
-            try:
-                res = client.recognize_general_with_options(recognize_general_request, runtime)
-                data = json.loads(res.body.data)
-                print(data['content'])
-            except Exception as error:
-                print(error.message)
-                print(error.data.get("Recommend"))
-                UtilClient.assert_as_string(error.message)
+                # print(i)
+                print(i.name)
+                print(kind)
+                # continue
+
+                client = Sample.create_client('LTAI5tBmKAjcNE7mpFiSW7n4', 'qOp2XtV5PQZjPh5O28YctTRb0x8B0n')
+                body_stream = StreamClient.read_from_file_path(i)
+                recognize_general_request = ocr_api_20210707_models.RecognizeGeneralRequest(
+                    body=body_stream
+                )
+                runtime = util_models.RuntimeOptions()
+                try:
+                    res = client.recognize_general_with_options(recognize_general_request, runtime)
+                    data = json.loads(res.body.data)
+                    content = data['content']
+                    l = content.split()
+                    print(l)
+                except Exception as error:
+                    print(error.message)
+                    print(error.data.get("Recommend"))
+                    UtilClient.assert_as_string(error.message)
 
 if __name__ == '__main__':
     Sample.main(sys.argv[1:])
