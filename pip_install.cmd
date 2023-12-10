38e6d4efe955899809de0f655509ee5fbd5ae977
Al Zee <z@alz.ee>
2023-12-10 19:53:12 +0800

    nth

--- pip_install.cmd
+++ pip_install.cmd
@@ -1,4 +1,4 @@
-pip install alibabacloud_ocr_api20210707==2.0.1
-pip install alibabacloud_tea_openapi
 pip install filetype
 pip install XlsxWriter
+pip install alibabacloud_darabonba_stream
+pip install alibabacloud_ocr_api20210707==2.0.1
--- pip_install.sh
+++ pip_install.sh
@@ -8,8 +8,8 @@
 
 ############### Main Part ###############
 
-pip install alibabacloud_ocr_api20210707==2.0.1
-pip install alibabacloud_tea_openapi
-# pip install alibabacloud_darabonba_stream
 pip install filetype
 pip install XlsxWriter
+pip install alibabacloud_darabonba_stream
+#pip install alibabacloud_tea_openapi
+pip install alibabacloud_ocr_api20210707==2.0.1
