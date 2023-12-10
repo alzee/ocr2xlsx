e5e435117ec9b88c8a1c4c7c77d857cc5f182007
Al Zee <z@alz.ee>
2023-12-11 01:34:11 +0800

    nth

--- ocr2xlsx.py
+++ ocr2xlsx.py
@@ -94,9 +94,11 @@ class Sample:
                 # print(kind)
                 # continue
 
-                client = Sample.create_client(os.getenv('KEYID'), os.getenv('KEYSECRET'))
+                client = Sample.create_client(os.getenv('ALI_KEY_ID'), os.getenv('ALI_KEY_SECRET'))
                 body_stream = StreamClient.read_from_file_path(file)
-                recognize_general_request = ocr_api_20210707_models.RecognizeGeneralRequest(
+                # recognize_request = ocr_api_20210707_models.RecognizeAllTextRequest(
+                recognize_request = ocr_api_20210707_models.RecognizeAdvancedRequest(
+                # recognize_request = ocr_api_20210707_models.RecognizeGeneralRequest(
                     body=body_stream
                 )
                 runtime = util_models.RuntimeOptions()
@@ -105,7 +107,9 @@ class Sample:
                 worksheet = workbook.add_worksheet()
 
                 try:
-                    res = client.recognize_general_with_options(recognize_general_request, runtime)
+                    # res = client.recognize_all_text_with_options(recognize_request, runtime)
+                    res = client.recognize_advanced_with_options(recognize_request, runtime)
+                    # res = client.recognize_general_with_options(recognize_request, runtime)
                     data = json.loads(res.body.data)
                     content = data['content']
                     # content = '目 待机 (00 DI ? 08/12/2023 00：40 样品状态 状态 详细信息 实时显示 分析仪状态 所有 快速 IS E 视图模式 静态 样本号 E001 血清 样品ID 6001 TP 49.6 ALB 34.6 ALT 64.8 AST 50.3 GGT 47.9 TB IL 19.3 DB IL 13.5 CHE 5929 ALP 121.9 UA 326.7 83.6 BUN 6.72 GLU 5.51 TG 1.48 2.70 CK 153.8 LDH 161.3 HB DH 171.1 AMY 83.9 LPS 44.3 样本号 E002 血清 样品ID 6002 TP 80.1 ALB 44.0 @ ALT 148.4 AST 149.6 GGT 199.4 TB IL 71.0 DB IL 34.8 CHE 9599 ALP 307.8 UA 662.2 517.3 BUN 20.16 GLU 13.16 TG 2.69 4.55 CK 277.1 LDH 300.2 HB DH 331.8 AMY 214.3 LPS 101.7 样本号 E003 血清 样品ID 6003 PA 196 APO A 0.91 APOB 0.44 RBP 27.3 A SO 105.11 样本号 E004 血清 样品ID 6004 PA 315 APO A 1.53 APOB 0.66 RBP 41.3 A SO 177.91 样本号 E005 血清 样品ID 6005 HDL 1.10 LDL 2.15 样本号 E006 血清 样品ID 6006 HDL 1.77 LDL 3.46 样本号 E007 血清 样品ID 6007 CK MB 51.5 样本号 E008 血清 样品ID 6008 CK MB 116.0 样本号 E009 血清 样品ID 6009 5NT 69.2 样本号 E010 血清 样品ID 6010 5NT 89.3 二 转换至 反应 搜索 静态视图 样品管理器 监控器 IS E保养 六 凤'
