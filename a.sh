0fde57cce4af4e4e26aef32cec13d16297118dcc
Al Zee <z@alz.ee>
2023-12-10 14:54:18 +0800

    nth

--- a.sh
+++ a.sh
@@ -10,5 +10,6 @@
 
 pip install alibabacloud_ocr_api20210707==2.0.1
 pip install alibabacloud_tea_openapi
-pip install filetype
 # pip install alibabacloud_darabonba_stream
+pip install filetype
+pip install XlsxWriter
